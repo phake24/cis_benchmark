62c6717d685ab40172e1e984f1295fac92ea21cf
Franz Josef <s.burger@telekom.de>
2022-10-25 08:36:09 +0200

    check if duplicated UIDs exist

---
+++ roles/cis_deb11/files/tmp/6.2.5.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# VPP Linux Hardening
+#
+# check if duplicated UIDs exist
+
+cut -f3 -d":" /etc/passwd | sort -n | uniq -c | while read x ; do
+ [ -z "$x" ] && break
+ set - $x
+ if [ $1 -gt 1 ]; then
+ users=$(awk -F: '($3 == n) { print $1 }' n=$2 /etc/passwd | xargs)
+ echo "Duplicate UID ($2): $users"
+ fi
+done
